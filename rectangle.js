362ef8ebfe6f7332f66f23237d28e7b90198ce62
yyaskriloff <69407772+yyaskriloff@users.noreply.github.com>
2022-03-03 01:02:23 +0200

    little changes

--- rectangle.js
+++ rectangle.js
@@ -1,7 +1,7 @@
 module.exports = class Rectangle {
-    constructor(width, height) {
-        this.width = width;
+    constructor(height, width) {
         this.height = height;
+        this.width = width;
     }
     get area() { return this.width * this.height }
     get diagonal() { return Math.sqrt((this.width ** 2) + (this.height ** 2)) }
--- test.js
+++ test.js
@@ -2,14 +2,18 @@ const Rectangle = require("./rectangle")
 
 const example = new Rectangle(3, 4)
 
-test(`the area of a rectangle that is ${example.width} by ${example.height}`, () => {
+test(`the height of the rectangle is ${example.height} and the width is ${example.width}`, () => {
+    expect(example).toEqual({ height: 3, width: 4 });
+})
+
+test(`the area of a rectangle that is ${example.width} by ${example.height} = 12`, () => {
     expect(example.area).toBe(12);
 })
 
-test(`the diagnal of a rectangle that is ${example.width} by ${example.height}`, () => {
+test(`the diagnal of a rectangle that is ${example.width} by ${example.height} = 5`, () => {
     expect(example.diagonal).toBe(5);
 })
 
-test(`the perimeter of a rectangle that is ${example.width} by ${example.height}`, () => {
+test(`the perimeter of a rectangle that is ${example.width} by ${example.height} = 14`, () => {
     expect(example.perimeter).toBe(14);
 })
